90da1c823f18a9901e20c210f547b4b2f2be6a4f
Jardeylson jacinto <jardeylsong.m@gmail.com>
2022-10-06 10:19:26 -0300

    feat: second test of billing cycle

--- config/database.js
+++ config/database.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
 
-mongoose.Error.messages.general.required = "o atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.general.required = "o atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min =
+  "o '{VALUE}' informado é menor do que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max =
+  "o '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum =
+  "O '{VALUE}' não é válido para o atributo '{PATH}'."

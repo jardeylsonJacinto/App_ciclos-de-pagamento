6d6707dffd9e81c26698402f2d175c059a4d7367
Jardeylson jacinto <jardeylsong.m@gmail.com>
2022-10-06 14:47:25 -0300

    feat: payment cycle counter service

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+BillingCycle.route('count', function (request, response, next) {
+  BillingCycle.count(function (error, value) {
+    if (error) {
+      response.status(500).json({ errors: [error] })
+    } else {
+      response.json({ value })
+    }
+  })
+})
+
 module.exports = BillingCycle
